3d3cbd08d137105cf6a9009d1b990755fcf5c79c
John Martinez <jmartinez729@gmail.com>
2018-10-24 17:24:19 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  counter = number 
+  
+  while counter > 0 
+    puts "#{counter} SECOND(S)!"
+    #return "#{counter} SECOND(S)!"
+    counter -= 1 
+    sleep 1
+  end 
+  #puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
 end
